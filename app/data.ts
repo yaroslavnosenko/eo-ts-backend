cb145a0fe6c7406df251a14aef28a2bc7845bdad
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2021-05-09 16:28:15 +0300

    Organizations and participants

--- app/data.ts
+++ app/data.ts
@@ -1,6 +1,41 @@
-import { DataFrame } from 'dataframe-js'
+import { parse } from 'csv/lib/sync'
+import * as fs from 'fs'
 
-export const dataFrame = async () => {
-  const data = await DataFrame.fromText('http://localhost:8080/static/data.csv')
+export const getAuctions = (): any[] => {
+  const testData = fs.readFileSync('./data/auctions.csv', {
+    encoding: 'utf8',
+    flag: 'r',
+  })
+  let data = parse(testData, {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter: ';',
+    trim: true,
+  })
+
+  data = data.map((row) => {
+    delete row['auctionNo']
+    delete row['auctionId']
+    return row
+  })
+  return data
+}
+
+export const getOffers = (): any[] => {
+  const testData = fs.readFileSync('./data/offers.csv', {
+    encoding: 'utf8',
+    flag: 'r',
+  })
+  let data = parse(testData, {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter: ';',
+    trim: true,
+  })
+
+  data = data.map((row) => {
+    delete row['auctionId']
+    return row
+  })
   return data
 }
--- app/index.ts
+++ app/index.ts
@@ -1,32 +1,65 @@
 import * as express from 'express'
 import { Application, Request, Response } from 'express'
-import { dataFrame } from './data'
+import { getOffers, getAuctions } from './data'
 
 const app: Application = express()
 
-app.get('/fields', async (req: Request, res: Response) => {
-  const data = await dataFrame()
-  return res.json(data.listColumns())
+app.get('/organizations', (req: Request, res: Response) => {
+  const auctions = getAuctions()
+  const organizations = auctions.map((row) => row['organization'])
+  return res.json(Array.from(new Set(organizations)))
 })
 
-app.get('/fields/:fieldName/unique', async (req: Request, res: Response) => {
-  const { fieldName } = req.params
-  const data = await dataFrame()
-  const query = JSON.parse(data.unique(fieldName).toJSON())
-  return res.json(query[fieldName])
+app.get('/organizations/:orgId', (req: Request, res: Response) => {
+  const auctions = getAuctions()
+  const { orgId } = req.params
+  const myAuctions = auctions.filter((row) => row['organization'] === orgId)
+  const wonAuctions = myAuctions.filter((row) => row['winner'] !== '0')
+  const jsonRes = {
+    organization: orgId,
+    allWiners: Array.from(new Set(myAuctions.map((row) => row['winner']))),
+    categories: Array.from(new Set(myAuctions.map((row) => row['category']))),
+    totalSpending: wonAuctions
+      .map((row) => parseFloat(row['winnerOffer'].replace(',', '.')))
+      .reduce((a, b) => a + b, 0),
+    auctionsCount: myAuctions.length,
+    wonAuctionsCount: wonAuctions.length,
+    auctions: myAuctions,
+  }
+  return res.json(jsonRes)
 })
 
-app.get('/auctions/:auctionId', async (req: Request, res: Response) => {
-  const { auctionId } = req.params
-  const data = await dataFrame()
-  const query = JSON.parse(
-    data.filter((row) => row.get('UniqueID') === auctionId).toJSON()
+app.get('/participants', (req: Request, res: Response) => {
+  const offers = getOffers()
+  const participants = offers.map((row) => row['participant'])
+  return res.json(Array.from(new Set(participants)))
+})
+
+app.get('/participants/:partId', (req: Request, res: Response) => {
+  const offers = getOffers()
+  const auctions = getAuctions()
+  const { partId } = req.params
+  const myOffers = offers.filter((row) => row['participant'] === partId)
+  const myAuctionsIds = Array.from(
+    new Set(myOffers.map((row) => row['auctionUniqueId']))
   )
-  console.log(query)
-  return res.json(query)
+  const myAuctions = auctions.filter((row) =>
+    myAuctionsIds.includes(row['auctionUniqueId'])
+  )
+  const wonAuctions = myAuctions.filter((row) => row['winner'] === partId)
+  const jsonRes = {
+    participant: partId,
+    categories: Array.from(new Set(myAuctions.map((row) => row['category']))),
+    totalIncome: wonAuctions
+      .map((row) => parseFloat(row['winnerOffer'].replace(',', '.')))
+      .reduce((a, b) => a + b, 0),
+    auctionsCount: myAuctions.length,
+    wonAuctionsCount: wonAuctions.length,
+    auctions: myAuctions,
+  }
+  return res.json(jsonRes)
 })
 
-app.use('/static', express.static('data'))
 app.listen(8080, () => {
   console.log(`server started at http://localhost:8080}`)
 })
